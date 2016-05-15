a72e5ebfdca009b07024803e18bda244451ad04b
Stav Gold <ragaxus@gmail.com>
2016-05-14 22:46:54 -0500

    "created prefect view; prefect view sends post requests to server; server updates scores based on prefect posts"

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
 //var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -17,8 +17,8 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,28 @@ router.get('/', function(req, res, next) {
     res.render('index', {teamdata: teamdata});
 });
 
-module.exports = router;
+// GET prefect score updating window.
+router.get('/prefect', function(req, res, next) {
+    res.render('prefect');
+});
+
+// POST new score results to the server.
+router.post('/prefect', function(req,res) {
+    var data = JSON.parse(req.body.data);
+    console.log(JSON.stringify(data)+" is my JAM THO");
+    //fs.writeFile("testFile",data, function(err) {
+	//if(err) {
+	    //return console.log(err);
+	//}
+    //});
+    console.log("data consists of "+data.length+" elements.");
+    data.forEach(function(val) {
+	if (teamdata[val.TeamName]) {
+	    console.log(teamdata[val.TeamName].CurrentPts)
+	    teamdata[val.TeamName].CurrentPts += parseInt(val.Modifier);
+	}
+    });
+    res.send("");
+});
+
+    module.exports = router;
