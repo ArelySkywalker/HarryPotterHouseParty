d26fed6a4954430795757f3c3cff019634ceee73
Lnk2128 <Lnk2128@gmail.com>
2016-05-21 18:45:19 -0500

    changed foreach to for

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,8 @@ router.post('/prefect', function(req,res) {
 	//}
     //});
     console.log("data consists of "+data.length+" elements.");
-    data.forEach(function(val) {
+    for (i = 0; i < data.length; i++) {
+    	val = data[i];
     	console.log("This is inside the foreach loop. Printing val:");
     	console.log(val);
 	if (teamdata[val.TeamName]) {
