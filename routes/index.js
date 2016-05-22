d1eba46c8b5bc297ee2e3e5f4a82072a44a1a47f
Lnk2128 <Lnk2128@gmail.com>
2016-05-22 14:00:29 -0500

    Replaced someObj with teamdata

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var fs = require('fs');
-var someObj = JSON.parse(fs.readFileSync('teamdata.json', 'utf8'));
+var teamdata = JSON.parse(fs.readFileSync('teamdata.json', 'utf8'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 //  res.render('index', { title: 'Express' });
-    res.render('index', {teamdata: someObj});
+    res.render('index', {teamdata: teamdata});
 });
 
 // GET prefect score updating window.
