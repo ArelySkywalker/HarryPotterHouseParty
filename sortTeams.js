e46f88f6c808eaff5d3ad85ddecef2b05df82492
Stav Gold <ragaxus@gmail.com>
2016-05-27 17:15:32 -0500

    Ari styling + pictures

---
+++ sortTeams.js
@@ -0,0 +1,12 @@
+
+//-             - 
+//-                    var teamArray = [];
+//-                    for (var name in teamdata) {
+//-                        var teamArrObj = teamdata[name];
+//-                        teamArrObj.name = name;
+//-                        teamArray.push(teamArrObj);
+//-                    }
+//-                    teamArray.sort(function(a,b){
+//-                        return b.CurrentPts - a.CurrentPts;
+//-                        }
+//-                    );
